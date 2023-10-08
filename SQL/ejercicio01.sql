681253da5f1107c110bef04953ec536f0be7b4b1
quimcomasn <qcomas2002@gmail.com>
2023-10-08 14:28:29 +0200

    Ej 1 y 2

--- SQL/ejercicio01.sql
+++ SQL/ejercicio01.sql
@@ -63,24 +63,65 @@ CREATE TABLE IF NOT EXISTS productos(
 /*
 10. Insertar varios productos en la tabla "Productos" con diferentes valores.
 */
-
+INSERT INTO public.productos(id,nombre,precio)
+VALUES(1,'Camiseta',55.2),(2,'Gorra',10.9),(3,'Zapatos',25.0);
 /*
 11. Consultar todos los clientes de la tabla "Clientes".
 */
-
+SELECT * FROM clientes;
 /*
 12. Consultar todos los pedidos de la tabla "Pedidos" junto con los nombres de los
 clientes correspondientes.
 */
-
+SELECT * FROM public.pedidos
+LEFT JOIN public.clientes
+ON public.pedidos.id = public.clientes.id
 /*
-
+13. Consultar los productos de la tabla "Productos" cuyo precio sea mayor a $50.
 */
-
+SELECT * FROM public.productos
+WHERE precio > 50;
 /*
-
+14. Consultar los pedidos de la tabla "Pedidos" que tengan una cantidad mayor o
+igual a 5.
 */
-
+SELECT * FROM public.pedidos
+WHERE cantidad >= 5;
 /*
-
+15. Consultar los clientes de la tabla "Clientes" cuyo nombre empiece con la letra "A".
 */
+SELECT * FROM public.clientes
+WHERE nombre LIKE 'A%';
+/*
+16. Realizar una consulta que muestre el nombre del cliente y el total de pedidos
+realizados por cada cliente.
+*/
+SELECT clientes.nombre, COUNT(pedidos.*) as num_total_pedidos FROM public.clientes
+INNER JOIN public.pedidos ON clientes.id = pedidos.cliente_id
+GROUP BY clientes.nombre;
+/*
+17. Realizar una consulta que muestre el nombre del producto y la cantidad total de
+pedidos de ese producto.
+*/
+SELECT productos.nombre, COUNT(pedidos.*) as num_total_pedidos FROM public.productos
+INNER JOIN public.pedidos ON productos.nombre = pedidos.producto
+GROUP BY productos.nombre;
+/*
+18. Agregar una columna llamada "fecha" a la tabla "Pedidos" de tipo fecha.
+*/
+ALTER TABLE public.pedidos
+ADD fecha DATE;
+/*
+19. Agregar una clave externa a la tabla "Pedidos" que haga referencia a la tabla
+"Productos" en la columna "producto".
+*/
+ALTER TABLE public.pedidos
+/*Se ha cambiado previamente el campo producto y ahroa recibe la ID del producto y no el nombre de el producto*/
+ADD FOREIGN KEY (producto) REFERENCES productos(id);
+/*
+20. Realizar una consulta que muestre los nombres de los clientes, los nombres de
+los productos y las cantidades de los pedidos donde coincida la clave externa.
+*/
+SELECT clientes.nombre, productos.nombre, pedidos.cantidad FROM public.clientes
+INNER JOIN pedidos ON clientes.id = pedidos.id
+INNER JOIN productos ON pedidos.producto = productos.id;
---
+++ SQL/ejercicio02.sql
@@ -0,0 +1,57 @@
+/*Ejercicio 2
+1. Crea una base de datos llamada "MiBaseDeDatos".
+*/
+CREATE DATABASE MiBaseDeDatos;
+/*
+2. Crea una tabla llamada "Usuarios" con las columnas: "id" (entero, clave
+primaria), "nombre" (texto) y "edad" (entero).
+*/
+CREATE TABLE IF NOT EXISTS Usuarios (
+	id int PRIMARY KEY,
+	nombre VARCHAR(255),
+	edad INT
+)
+/*
+3. Inserta dos registros en la tabla "Usuarios".
+*/
+INSERT INTO public.usuarios
+VALUES(1, 'Quim', 20),(2, 'Pablo', 34)
+/*
+4. Actualiza la edad de un usuario en la tabla "Usuarios"
+*/
+UPDATE public.usuarios
+SET edad = 29
+WHERE id = 2;
+/*
+5. Elimina un usuario de la tabla "Usuarios"
+*/
+DELETE FROM public.usuarios
+WHERE id = 2;
+/*
+1. Crea una tabla llamada "Ciudades" con las columnas: "id" (entero, clave
+primaria), "nombre" (texto) y "pais" (texto).
+*/
+CREATE TABLE IF NOT EXISTS Ciudades(
+	id int PRIMARY KEY,
+	nombre VARCHAR(255),
+	pais VARCHAR(255)
+)
+/*
+2. Inserta al menos tres registros en la tabla "Ciudades".
+*/
+INSERT INTO public.ciudades
+VALUES(1,'Barcelona','España'),(2,'Paris', 'Francia'),(3,'Berlín','Alemania')
+/*
+3. Crea una foreign key en la tabla "Usuarios" que se relacione con la columna "id"
+de la tabla "Ciudades".
+*/
+
+/*
+4. Realiza una consulta que muestre los nombres de los usuarios junto con el
+nombre de su ciudad y país (utiliza un LEFT JOIN).
+*/
+
+/*
+5. Realiza una consulta que muestre solo los usuarios que tienen una ciudad
+asociada (utiliza un INNER JOIN).
+*/
