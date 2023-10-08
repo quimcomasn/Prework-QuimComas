aff40c73b75f51c90561c906cd9e017190860b67
quimcomasn <qcomas2002@gmail.com>
2023-10-08 17:23:43 +0200

    Update ejercicio02.sql

--- SQL/ejercicio02.sql
+++ SQL/ejercicio02.sql
@@ -45,7 +45,9 @@ VALUES(1,'Barcelona','España'),(2,'Paris', 'Francia'),(3,'Berlín','Alemania')
 3. Crea una foreign key en la tabla "Usuarios" que se relacione con la columna "id"
 de la tabla "Ciudades".
 */
-
+ALTER TABLE public.usuarios
+ADD CONSTRAINT fk_ciudades
+FOREIGN KEY (id) REFERENCES ciudades(id);
 /*
 4. Realiza una consulta que muestre los nombres de los usuarios junto con el
 nombre de su ciudad y país (utiliza un LEFT JOIN).
@@ -55,3 +57,5 @@ nombre de su ciudad y país (utiliza un LEFT JOIN).
 5. Realiza una consulta que muestre solo los usuarios que tienen una ciudad
 asociada (utiliza un INNER JOIN).
 */
+SELECT ciudades.nombre, usuarios.nombre FROM public.usuarios
+INNER JOIN public.ciudades ON usuarios.id = ciudades.id;
